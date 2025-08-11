c527d52917754f40c03e49466a1aa12fd22c8631
ojiwzrd <ojiwzrd@gmail.com>
2025-08-12 02:59:19 +0700

    first commit

--- config/settings.js
+++ config/settings.js
@@ -1,3 +1,3 @@
 global.author = 'mewzrd'
 global.community = 'https://chat.whatsapp.com/Cr5mLtlRAdl0aRDYthz1Zj?mode=ac_t'
-global.bottoken = '8272460260:AAG9xI6uysex2cSt0bX3wluGOA-9DWuZAko'
+global.bottoken = 'YOUR_BOT_TOKEN'
